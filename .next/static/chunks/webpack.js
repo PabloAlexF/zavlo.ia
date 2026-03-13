a2ab1f9384e0ce637b1e39bb460afaf1e5ee5027
PabloAnrade <Pablolexandrade@gmail.com>
2026-03-12 21:26:30 -0300

    fix page bot

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5338d7fbba46c415"; }
+/******/ 		__webpack_require__.h = function() { return "69d473afcca08c33"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
