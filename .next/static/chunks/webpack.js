094a82da9b31c33eafb1a53ae4581db6b5a28b8d
PabloAnrade <Pablolexandrade@gmail.com>
2026-03-14 13:55:03 -0300

    corrections front

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b4eb196150ef892"; }
+/******/ 		__webpack_require__.h = function() { return "ed8574d0df0c538e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
