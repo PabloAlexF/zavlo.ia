7864bb582d102310f8df56edd2393a48f8225aa5
PabloAnrade <Pablolexandrade@gmail.com>
2026-03-19 14:08:55 -0300

    awdawdawdwad

--- src/modules/scraping/scraping.module.ts
+++ src/modules/scraping/scraping.module.ts
@@ -4,25 +4,25 @@ import { ScrapingController } from './scraping.controller';
 import { GoogleShoppingService } from './google-shopping.service';
 import { GoogleLensService } from './google-lens.service';
 import { OlxService } from './olx.service';
-import { MobiautoService } from './mobiauto.service';
 import { WebmotorsService } from './webmotors.service';
+import { MercadoLivreService } from './mercadolivre.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [ScrapingController],
   providers: [
-    GoogleShoppingService, 
-    GoogleLensService, 
-    OlxService, 
-    MobiautoService, 
-    WebmotorsService
+    GoogleShoppingService,
+    GoogleLensService,
+    OlxService,
+    WebmotorsService,
+    MercadoLivreService,
   ],
   exports: [
-    GoogleShoppingService, 
-    GoogleLensService, 
-    OlxService, 
-    MobiautoService, 
-    WebmotorsService
+    GoogleShoppingService,
+    GoogleLensService,
+    OlxService,
+    WebmotorsService,
+    MercadoLivreService,
   ],
 })
 export class ScrapingModule {}
